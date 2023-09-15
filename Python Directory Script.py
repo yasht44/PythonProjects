7bb016a45148f8e0d9c3729ffc8609837e4c25f7
Yash T <yash.tewani.nyc@gmail.com>
2023-09-15 12:53:24 -0400

    changes 3

--- Python Directory Script.py
+++ Python Directory Script.py
@@ -1,4 +1,5 @@
-import os #allows us to acess OS information 
+import os
+from datetime import datetime
 
 def list_files_in_directory(path=None):
     if path is None:
@@ -12,7 +13,7 @@ def list_files_in_directory(path=None):
                 file_path = os.path.join(root, filename)
                 file_info = {
                     "size_bytes": os.path.getsize(file_path),
-                    "modification_time": os.path.getmtime(file_path),
+                    "modification_time": datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d %H:%M:%S'),
                 }
                 file_dict[file_path] = file_info
 
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     custom_path = input("Enter the directory path to analyze (leave blank for current directory): ").strip()
 
     if custom_path == "":
-        custom_path = os.getcwd()  # Use current directory if input is blank
+        custom_path = os.getcwd()  # Use the current directory if input is blank
 
     # Call the function to list files and store the result in a variable
     files_info = list_files_in_directory(custom_path)
@@ -37,6 +38,4 @@ if __name__ == "__main__":
             for key, value in file_info.items():
                 print(key + ":", value)
     else:
-        print("No files found in the specified directoryy.")
-
-    
+        print("No files found in the specified directory.")
